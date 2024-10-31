82dbe9bcb6e9feadf5c21fbfce9d9f6089d7dc48
Dhruv Lathia <104671436+owncrafts@users.noreply.github.com>
2024-10-31 21:57:12 +0530

    done 5

--- bot.py
+++ bot.py
@@ -40,14 +40,12 @@ from handlers.save_media import (
 
 MediaList = {}
 
-print("API_HASH:", Config.API_HASH) 
-
 Bot = Client(
-    name=Config.BOT_USERNAME,
-    in_memory=True,
+    # name=Config.BOT_USERNAME,
+    # in_memory=True,
     bot_token=Config.BOT_TOKEN,
     api_id=Config.API_ID,
-    # api_hash=Config.API_HASH
+    api_hash=Config.API_HASH
 )
 
 
